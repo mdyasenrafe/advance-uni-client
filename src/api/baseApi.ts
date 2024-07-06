45fa25bd52f0bbd44cedca5cc27d1c7e75461631
Md yasen rafe <mdyasenrafe@gmail.com>
2024-07-06 23:44:34 +0600

    refresh token issue fixed

--- src/api/baseApi.ts
+++ src/api/baseApi.ts
@@ -1,5 +1,13 @@
-import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import {
+  BaseQueryApi,
+  BaseQueryFn,
+  DefinitionType,
+  FetchArgs,
+  createApi,
+  fetchBaseQuery,
+} from "@reduxjs/toolkit/query/react";
 import { RootState } from "../redux/store";
+import { logout, setUsers } from "../redux/features/auth/authSlice";
 
 const baseQuery = fetchBaseQuery({
   baseUrl: "https://advance-uni-server.vercel.app/api/v1",
@@ -7,15 +15,41 @@ const baseQuery = fetchBaseQuery({
   prepareHeaders: (headers, { getState }) => {
     const token = (getState() as RootState).auth.token;
     if (token) {
-      headers.set("authorization", `Bearer ${token}`);
+      headers.set("authorization", `${token}`);
     }
 
     return headers;
   },
 });
 
+const baseQueryWithRefreshToken: BaseQueryFn<
+  FetchArgs,
+  BaseQueryApi,
+  DefinitionType
+> = async (args, api, extraOptions): Promise<any> => {
+  let result = await baseQuery(args, api, extraOptions);
+
+  if (result.error?.status === 401) {
+    fetch("https://advance-uni-server.vercel.app/api/v1/auth/refresh-token", {
+      method: "POST",
+      credentials: "include",
+    })
+      .then((res) => res.json())
+      .then(async (data) => {
+        if (data?.data?.accessToken) {
+          const user = (api.getState() as RootState).auth.user;
+          api.dispatch(setUsers({ user, token: data.data.accessToken }));
+          let result = await baseQuery(args, api, extraOptions);
+        } else {
+          api.dispatch(logout());
+        }
+      });
+  }
+  return result;
+};
+
 export const baseApi = createApi({
   reducerPath: "baseapi",
-  baseQuery: baseQuery,
+  baseQuery: baseQueryWithRefreshToken,
   endpoints: () => ({}),
 });
--- src/pages/AcademicSemester/AcaemicSemester.tsx
+++ src/pages/AcademicSemester/AcaemicSemester.tsx
@@ -1,5 +1,8 @@
 import React from "react";
+import { useGetAllAcademicSemesterQuery } from "../../redux/features/academicSemster/academicSemesterApi";
 
 export const AcaemicSemester = () => {
+  const { data } = useGetAllAcademicSemesterQuery(undefined);
+  console.log(data);
   return <div>AcaemicSemester</div>;
 };
